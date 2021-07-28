aecd5b11ef144133a06614a22a29cc8985914841
hoyuhub <hoyu_z@outlook.com>
2021-07-28 22:58:59 +0800

    fix:对应数据库字段

--- Infrastructure/RBAC.Infrastructure.Repository/Migrations/20210728145729_updateColumn.Designer.cs
+++ Infrastructure/RBAC.Infrastructure.Repository/Migrations/20210728145729_updateColumn.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RBAC.Infrastructure.Repository;
 
 namespace RBAC.Infrastructure.Repository.Migrations
 {
     [DbContext(typeof(RBACDBContext))]
-    partial class RBACDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210728145729_updateColumn")]
+    partial class updateColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
